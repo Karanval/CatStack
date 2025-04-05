c4d408f182c9bd330ea3e0df81fe58608dc29126
Jacob Kaae Olesen <spjak1@gmail.com>
2025-04-05 21:23:46 +0200

    Visualized ammo

--- Scenes/AmmoCounter/AmmoCounter.gd
+++ Scenes/AmmoCounter/AmmoCounter.gd
@@ -1,7 +1,20 @@
-extends CenterContainer
+extends Control
+
+var ammo_scene = preload("res://Scenes/AmmoCounter/Ammo.tscn")
 	
 func _ready() -> void:
 	GameManager.ammo_changed.connect(update_ammo_count)
 	
 func update_ammo_count(count: int):
-	$AmmoText.text = "Ammo: " + str(count)
+	remove_all_ammo()
+	for a in range(count):
+		add_one_ammo()
+	
+func remove_all_ammo():
+	var ammos = $MarginContainer/HBoxContainer.get_children()
+	for a in ammos:
+		$MarginContainer/HBoxContainer.remove_child(a)
+		
+func add_one_ammo():
+	var ammo = ammo_scene.instantiate()
+	$MarginContainer/HBoxContainer.add_child(ammo)

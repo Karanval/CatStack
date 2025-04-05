1f961af435493e45b7cd00712461724a5f62f429
Jacob Kaae Olesen <spjak1@gmail.com>
2025-04-05 13:34:40 +0200

    Added simple bullet

---
+++ Scenes/Bullet/Bullet.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var DAMAGE: int
+@export var DIRECTION: Vector2
+@export var SPEED: int
+@export var CAN_HIT_OBJECTS: bool
+
+signal damage_inflicted
+
+var _velocity
+
+func _ready() -> void:
+	_velocity = DIRECTION.normalized() * SPEED
+	
+func _physics_process(delta: float) -> void:
+	self.position += _velocity * delta
+
+# Bullet hits a monster
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	emit_signal("damage_inflicted", body, DAMAGE)
+	queue_free()

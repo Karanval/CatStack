4a2fe842a31e5693e4d035b58aa4a44abab45a15
Andrew <abcfantasy@gmail.com>
2025-04-05 15:50:51 +0200

    Add monsters

---
+++ Scenes/Monsters/ghost_monster.gd
@@ -0,0 +1,13 @@
+extends BaseMonster
+
+var movementTime := 0.0
+
+var direction: Vector2
+
+func _ready() -> void:
+	direction = (global_position - target.global_position).normalized()
+	
+func get_velocity(delta: float):
+	movementTime += delta
+	#return Vector2(SPEED * speed_modifier * delta, )
+	return Vector2(SPEED * direction.x * speed_modifier * delta, SPEED * direction.y * speed_modifier * delta)

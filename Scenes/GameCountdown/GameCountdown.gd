15b0e1ee422db5b2ac2b560b488be1a95f486231
Jacob Kaae Olesen <spjak1@gmail.com>
2025-04-06 13:38:28 +0200

    Updated UI elements layout

--- Scenes/GameCountdown/GameCountdown.gd
+++ Scenes/GameCountdown/GameCountdown.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var _time_left: int
+const SCORE_SCALING := 2
 
 func _ready() -> void:
 	_time_left = GameManager.GAME_LENGTH
@@ -9,10 +10,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var new_time = int(floor($GameCountdown.time_left))
 	if new_time != _time_left:
-		GameManager.changeScore(_time_left - new_time)
+		GameManager.changeScore((_time_left - new_time)*SCORE_SCALING)
 		_time_left = new_time
-		$TimerText.text = str(_time_left)
+		$TimerText.text = _convert_to_minutes_text(_time_left)
 
+func _convert_to_minutes_text(time: int) -> String:
+	var minutes = floor(time/60)
+	var seconds = time - (minutes*60)
+	return str(minutes) + ":" + str(seconds)
 
 func _on_game_countdown_timeout() -> void:
 	GameManager.endGame()

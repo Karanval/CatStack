b43b32a5f4cc43b8f553a450412447c5228b534e
Jacob Kaae Olesen <spjak1@gmail.com>
2025-04-06 14:04:39 +0200

    Change face depending on result

--- Scenes/GameOver/GameOver.gd
+++ Scenes/GameOver/GameOver.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 var game_scene = preload("res://Scenes/TestScene/TestScene.tscn")
+var happy_face = load("res://graphics/main_character/mc_happy_face.png")
+var angry_face = load("res://graphics/main_character/mc_angry_face.png")
 
 func _ready() -> void:
 	$CanvasLayer/Control/ScoreLabel.text = "Score: " + str(GameManager.getScore())
+	if GameManager.win:
+		$PlayerCharacter/sprites/body/head/face.texture = happy_face
+	else:	
+		$PlayerCharacter/sprites/body/head/face.texture = angry_face
 
 func _on_restart_button_button_down() -> void:
 	GameManager.resetScore()
